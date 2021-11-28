79d6518a2bff3786ddf116fcc50897522828a3ae
jongwoo <leejongwoo.dev@gmail.com>
2021-11-28 10:43:57 +0900

    init commit

---
+++ src/main/java/com/code/bookStore/dto/BookDto.java
@@ -0,0 +1,16 @@
+package com.code.bookStore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+
+    private String title;
+
+}
